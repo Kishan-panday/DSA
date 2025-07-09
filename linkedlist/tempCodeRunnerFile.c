97d889ba718ac2a9176945bc6e5d206cd0e258b4
Kishan-Panday <kishanpandey4463@gmail.com>
2025-07-09 22:55:14 +0530

    Deletion at any index

--- linkedlist/tempCodeRunnerFile.c
+++ linkedlist/tempCodeRunnerFile.c
@@ -15,7 +15,7 @@ void printlinked(struct node *head){
 struct node *delete(struct node * head,int index){
     struct node *p = head;
     struct node *q=head ->next;
-     for (int i = 0; i < index-1; i++)
+     for (int i = 0; i < index; i++)
      {
         p = p->next;
         q= q->next;
@@ -37,7 +37,7 @@ int main(){
        second ->next = third;
        third -> data =40;
        third -> next = NULL;
-        delete(head,2);
+        delete(head,0);
         printlinked(head);
        return 0;
 }
