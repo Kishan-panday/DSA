b1ec42c774b75deacc274f1ff8f9bab15e97cfd9
Kishan-Panday <kishanpandey4463@gmail.com>
2025-07-17 00:36:15 +0530

    circular linkedlist deletion at index

---
+++ circular linked list/tempCodeRunnerFile.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node * next;
+};
+void print(struct node * head){
+    if (head==NULL)
+    {
+        printf("circular linked list is empty");
+    }
+    struct node *p = head;
+    do
+    {
+        printf("%d->",p->data);
+        p= p->next;
+    } while (p!=head);
+    printf("%d",head->data);
+}
+
+struct node * delete( struct node* head,int index){
+    struct node *p = head;
+    struct node *q = p->next;
+    for (int i = 0; i < index-1; i++)
+    {
+       p= p->next;
+       q= q->next;
+    }
+    p->next=q->next;
+    free(q);
+    return head;
+    
+}
+
+int main(){
+    struct node * head = (struct node*)malloc(sizeof(struct node));
+    struct node * first = (struct node*)malloc(sizeof(struct node));
+    struct node * second = (struct node*)malloc(sizeof(struct node));
+    struct node * third = (struct node*)malloc(sizeof(struct node));
+    head ->data = 10;
+    head ->next = first;
+    first-> data = 20;
+    first->next = second;
+    second -> data = 30;
+    second ->next= third;
+    third -> data=40;
+    third->next = head;
+    delete(head,3);
+    print(head);
+     return 0;
+}
